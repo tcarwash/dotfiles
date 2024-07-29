aa67ea4353ce018a9cae23b59fbc2036a9c3293a
Tyler F. Carr <tcarwash@gmail.com>
2024-07-28 20:54:39 -0700

    auto close stuff

--- init.lua
+++ init.lua
@@ -80,12 +80,16 @@ require('lazy').setup({
     {'williamboman/mason-lspconfig.nvim'},
     {'mbbill/undotree'},
     {'philrunninger/nerdtree-visual-selection'},
-    {'Xuyuanp/nerdtree-git-plugin'},
+    {
+        'altermo/ultimate-autopair.nvim',
+        event={'InsertEnter','CmdlineEnter'},
+        branch='v0.6',
+},
     {
         "ThePrimeagen/harpoon",
         branch = "harpoon2",
         dependencies = { 
-            "nvim-lua/plenary.nvim" 
+            "nvim-lua/plenary.nvim"
         }
     },
     {
@@ -103,8 +107,48 @@ require('lazy').setup({
         end,
       },
     },
-
-    })
+      {
+    'abecodes/tabout.nvim',
+    lazy = false,
+    config = function()
+      require('tabout').setup {
+        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true, -- shift content if tab out is not possible
+        act_as_shift_tab = false, -- reverse shift content if tab out is not possible (if your keyboard/terminal supports <S-Tab>)
+        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>', -- reverse shift default action,
+        enable_backwards = true, -- well ...
+        completion = false, -- if the tabkey is used in a completion pum
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = '`', close = '`' },
+          { open = '(', close = ')' },
+          { open = '[', close = ']' },
+          { open = '{', close = '}' }
+        },
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        exclude = {} -- tabout will ignore these filetypes
+      }
+    end,
+    dependencies = { -- These are optional
+      "nvim-treesitter/nvim-treesitter",
+      "L3MON4D3/LuaSnip",
+      "hrsh7th/nvim-cmp"
+    },
+    opt = true,  -- Set this to true if the plugin is optional
+    event = 'InsertCharPre', -- Set the event to 'InsertCharPre' for better compatibility
+    priority = 1000,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      -- Disable default tab keybinding in LuaSnip
+      return {}
+    end,
+  },
+})
 
 
 -- Theming
@@ -162,6 +206,8 @@ vim.g.blamer_enabled = 1
 vim.g.blamer_date_format = '%e %b %Y @ %H:%M'
 vim.cmd('highlight Blamer guifg=darkorange')
 
+-- AutoClose
+
 -- LSP
 local lsp_zero = require('lsp-zero')
 
@@ -196,6 +242,13 @@ vim.keymap.set("n", "<leader>g", function() harpoon:list():select(4) end)
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<leader>p", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<leader>n", function() harpoon:list():next() end)
+
+-- Ultimate AutoPair
+
+require'ultimate-autopair'.setup{}
+local ua=require'ultimate-autopair'
+local configs={ua.extend_default({})}
+ua.init(configs)
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
